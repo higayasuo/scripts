e3820974316a7aa5afc6b0ac70860f2c7bd200e7
Yasuo Higa <higayasuo@gmail.com>
2022-06-05 14:39:14 +0900

    Update cloud-config.yaml

---
+++ create_ubuntu.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+multipass launch -n ubuntu -d 50G -m 8G --cloud-init cloud-config.yaml
---
+++ install_id_rsa.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo install -m 600 -o $USER -g $(id -g) /var/root/Library/Application\ Support/multipassd/ssh-keys/id_rsa ~/.ssh/multipass.id_rsa
