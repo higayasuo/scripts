7b941180380b18fd41f0af9de51050c336c5c498
Yasuo Higa <higayasuo@gmail.com>
2024-05-06 13:46:01 +0900

    Fix cloud-config.yaml and install_id_rsa.sh

--- install_id_rsa.sh
+++ install_id_rsa.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo install -m 600 -o $USER -g $(id -g) /var/root/Library/Application\ Support/multipassd/ssh-keys/id_rsa ~/.ssh/multipass.id_rsa
+sudo install -m 600 -o $USER -g $(id -g) /var/root/Library/Application\ Support/multipassd/ssh-keys/id_rsa ~/.ssh/id_rsa
